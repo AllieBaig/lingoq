9aaf51ae9b69d9832de8c275034da15dff8a975e
AllieBaig <136852901+AllieBaig@users.noreply.github.com>
2025-06-07 22:54:35 +0100

    Create eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+
+// File: eslint.config.js
+import js from '@eslint/js';
+
+export default [
+    js.configs.recommended,
+    {
+        languageOptions: {
+            ecmaVersion: 2022,
+            sourceType: 'module',
+        },
+        rules: {
+            'no-unused-vars': 'warn',
+            'no-console': 'off',
+            'prefer-const': 'error',
+            'no-var': 'error',
+        },
+    },
+];
+
